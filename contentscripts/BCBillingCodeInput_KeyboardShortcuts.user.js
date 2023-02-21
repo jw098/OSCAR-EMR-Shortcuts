8028267d1ba7e3da458b885a05f8e697fe723a5c
Justin Wong <justin.wong098@gmail.com>
2023-02-20 21:32:22 -0800

    bcbillingcodeinput_KS: delete old  keyboard shortcuts to input billing codes.

--- contentscripts/BCBillingCodeInput_KeyboardShortcuts.user.js
+++ contentscripts/BCBillingCodeInput_KeyboardShortcuts.user.js
@@ -35,40 +35,19 @@ function billingCodeInputPage_KeydownListeners(billingCodeInput_keyboardShortcut
 		billingCodeInput_keyboardShortcuts.billingCodeInput_shortcut_continue_enabled;
 	const continue_keybinding = 
 		billingCodeInput_keyboardShortcuts.billingCodeInput_shortcut_continue_keybinding;
-	const officeVisitInputCode_enabled = 
-		billingCodeInput_keyboardShortcuts.billingCodeInput_shortcut_officeVisitInputCode_enabled;
-	const officeVisitInputCode_keybinding = 
-		billingCodeInput_keyboardShortcuts.billingCodeInput_shortcut_officeVisitInputCode_keybinding;
-	const teleVisitInputCode_enabled = 
-		billingCodeInput_keyboardShortcuts.billingCodeInput_shortcut_teleVisitInputCode_enabled;
-	const teleVisitInputCode_keybinding = 
-		billingCodeInput_keyboardShortcuts.billingCodeInput_shortcut_teleVisitInputCode_keybinding;
+
 	const setFocusDxCode_enabled = 
 		billingCodeInput_keyboardShortcuts.billingCodeInput_shortcut_setFocusDxCode_enabled;
 	const setFocusDxCode_keybinding = 
 		billingCodeInput_keyboardShortcuts.billingCodeInput_shortcut_setFocusDxCode_keybinding;	
 
 	window.addEventListener('keydown', function(theEvent) {
-		var theKey = theEvent.key;
-		var theAltKey = theEvent.altKey;
-		var theCtrlKey = theEvent.ctrlKey;
-		var theShiftKey= theEvent.shiftKey;
 		let theTarget;
 		switch(true){
 			case  (continue_enabled 
 				&& keybindingMatches(continue_keybinding, theEvent)):	
 				theTarget = document.evaluate("id('buttonRow')/td/input[@value='Continue']",document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
 				theTarget.click();
-				break;
-			case  (officeVisitInputCode_enabled 
-				&& keybindingMatches(officeVisitInputCode_keybinding, theEvent)):
-				inPersonVisit();
-				scrollToPageEnd_BCBillingConfirm();
-				break;
-			case  (teleVisitInputCode_enabled 
-				&& keybindingMatches(teleVisitInputCode_keybinding, theEvent)):
-				virtualVisit();
-				scrollToPageEnd_BCBillingConfirm();
 				break;
 			case  (setFocusDxCode_enabled 
 				&& keybindingMatches(setFocusDxCode_keybinding, theEvent)):
