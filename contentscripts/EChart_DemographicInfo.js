2bd880798906120cac49469674011a3f16199174
Justin Wong <justin.wong098@gmail.com>
2023-02-20 17:04:57 -0800

    echart_demographicinfo: rewrote the XHR function to parse with XPATh instead of regex. added HC type to header.

--- contentscripts/EChart_DemographicInfo.js
+++ contentscripts/EChart_DemographicInfo.js
@@ -12,8 +12,6 @@
 
 
 
-var demoArrayVal2 = []
-
 
 async function loadDemographicInfo(){
 	// if(window.location.href.indexOf("careconnect") != -1){
@@ -35,37 +33,37 @@ async function loadDemographicInfo(){
 		'Postal',
 		'Age',
 		'Health Ins',
-		'Phone(W)'
+		'Phone(W)',
+		'Health Card Type'
 	]
 
 	const theURL = getMasterDemographicURL();
-	const demoArrayVal = await getMeasures2(demographicArray, theURL);
-	// console.log(demographicArrayValues);
-	// for (j = 0; j < demographicArray.length; j++) {
-	// 	// await getMeasures2(demographicArray[j], theURL);
-	// 	getMeasures(demographicArray[j]);
-	// }
-	//alert(demoArrayVal)
+	const demoArrayVal = await getDemographicInfo(demographicArray, theURL);
+
 	var passPHN = demoArrayVal[7].substr(0, 10);
 	// localStorage.setItem("LinkPHN", passPHN); //STORE PHN
 	// GM.setValue("LinkPHN", passPHN);
 	// console.log(`Saved PHN to GM global values: ${passPHN}`);
 
-	var HCN = demoArrayVal[7]
+	const HCType = demoArrayVal[9];
+	console.log(HCType)
+	var HCN = demoArrayVal[7].trim();
+	console.log(HCN)
 	res = HCN.slice(0, 4)
 	res = res + ' ' + HCN.slice(4, 7)
 	res = res + ' ' + HCN.slice(7)
 	HCN = res
 	header.innerHTML = headerReserve
-	var headerExtra1 = 'Cell: '
+	var headerHomePhone = '<b>Home: </b>' + telLink(demoArrayVal[2]);
+	var headerCellPhone = '<b>Cell: </b>' + telLink(demoArrayVal[0]);
+	var headerWorkPhone = '<b>Work: </b>' + telLink(demoArrayVal[8]);
+	var headerAddress = '<b>Address: </b>' + demoArrayVal[3] + ', ' + demoArrayVal[4] + ', ' + demoArrayVal[5] + " &nbsp; ";
+	var headerPHN = '<b>PHN: </b>' + "<span class='copyable'>" + HCN + "</span>" + " &nbsp; ";
+	const headerHCType = highlightHealthCardType(HCType);
 	var headerExtra2 = 'Age: '
 	var headerExtra3 = 'File#: '
-	var headerExtra4 = 'PHN: '
-	var headerExtra5 = 'Address: '
-	var headerExtra6 = 'Work: '
-	var headerExtra7 = 'Home: '
 
-	header.innerHTML += (headerExtra7.bold() + telLink(demoArrayVal[2]) +  headerExtra1.bold() + telLink(demoArrayVal[0]) + headerExtra6.bold() + telLink(demoArrayVal[8]) + headerExtra5.bold() + demoArrayVal[3] + ', ' + demoArrayVal[4] + ', ' + demoArrayVal[5] + ' ' + headerExtra4.bold() + "<span class='copyable'>" + HCN + "</span>" 	+
+	header.innerHTML += (headerHomePhone  +  headerCellPhone  + headerWorkPhone  + headerAddress  + headerPHN + headerHCType + 
 		'<br>' +
 		' eMail: '.bold() + "<span class='copyable'>" + demoArrayVal[1] + "</span>" +
 		'&nbsp;' + 
@@ -83,9 +81,9 @@ async function loadDemographicInfo(){
 	);
 
 
-	document.getElementById("button13").onclick = ButtonFunction13;
-	document.getElementById("button13").setAttribute('style', 'font-size:10px; color:green;');
-	function ButtonFunction13() {
+	document.getElementById("button13").onclick = openCareConnect;
+	document.getElementById("button13").setAttribute('style', 'color:green;'); //font-size:10px; 
+	function openCareConnect() {
 		// window.open("https://health.careconnect.ca?" + passPHN, "newWindow", target = "_blank")
 		window.open("https://health.careconnect.ca", "newWindow", target = "_blank")
 	}
@@ -102,8 +100,8 @@ async function loadDemographicInfo(){
 		document.body.appendChild(textArea);
 		textArea.select();
 		document.execCommand('copy');
-		$(this).css('background-color', 'pink')
-		textArea.setAttribute('style', 'position:absolute; top:32px; left:660px; width:250px; height:20px; font-size:13px; resize:none;');
+		$(this).css('background-color', '#c1a7f1');
+		textArea.setAttribute('style', 'position:absolute; top:32px; left:660px; width:150px; height:20px; font-size:13px; resize:none;');
 		textArea.setAttribute('title', 'Clipboard');
 	//	textArea.remove();  // to hide text area
 	})
@@ -111,6 +109,15 @@ async function loadDemographicInfo(){
 
 	// $('#enTemplate').width("160px"); //widens search field
 
+}
+
+function highlightHealthCardType(HCType){
+	if (HCType == "OT" || HCType == "US"){
+		return `<span id='hcType' style='background-color: #da4d4d'><b>HC Type: ${HCType}</b></span>` ;
+	}
+	else {
+		return `<span id='hcType'><b>HC Type: </b>${HCType}</span>`;
+	}
 }
 
 function getMasterDemographicURL(){
@@ -139,86 +146,63 @@ function loadPHNToCareConnect(){
 	  })();
 }
 
-async function getMeasures2(demographicArray, URL){
+
+
+
+async function getDemographicInfo(demographicArray, URL){
 	const otherPageXMLText = await getXMLHTTP(URL);
-	const str = otherPageXMLText;
-	// const otherPageHTML = new DOMParser().parseFromString(otherPageXMLText, "text/html");
+	// const str = otherPageXMLText;
+	const otherPageHTML = new DOMParser().parseFromString(otherPageXMLText, "text/html");
 
 	let demoArrayValues = [];
 	for (let i = 0; i < demographicArray.length; i++){
-		const measure = demographicArray[i];
+		const measureKey = demographicArray[i];
+		const measureHTMLKey = otherPageHTML.evaluate(`//span[contains(.,'${measureKey}')]`,otherPageHTML,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
+		
+		const measureHTMLVal = measureHTMLKey.nextElementSibling;
+		const measureVal = measureHTMLVal.innerText;
+		demoArrayValues.push(measureVal);
 
-		switch (measure) {
-			case "Phone(H)":
-				var myReString = '<span class="label">[\n\r\t]*\s*' + 'Phone' + '[(][' + 'H' + '][)]' + '(.|[\n])*'
-				break;
-			case "Phone(W)":
-				var myReString = '<span class="label">[\n\r\t]*\s*' + 'Phone' + '[(][' + 'W' + '][)]' + '(.|[\n])*'
-				break;
-			default:
-				var myReString = '<span class="label">[\n\r\t]*\s*' + measure + '(.|[\n])*'
-		}
+		// console.log(measureVal);
+
+		// switch (measureKey) {
+		// 	case "Phone(H)":
+		// 		var myReString = '<span class="label">[\n\r\t]*\s*' + 'Phone' + '[(][' + 'H' + '][)]' + '(.|[\n])*'
+		// 		break;
+		// 	case "Phone(W)":
+		// 		var myReString = '<span class="label">[\n\r\t]*\s*' + 'Phone' + '[(][' + 'W' + '][)]' + '(.|[\n])*'
+		// 		break;
+		// 	default:
+		// 		var myReString = '<span class="label">[\n\r\t]*\s*' + measureKey + '(.|[\n])*'
+		// }
 	
-		var myRe = new RegExp(myReString, 'g');
-		var myArray
-		while ((myArray = myRe.exec(str)) !== null) {
-			const y = myArray.toString()
-			//alert(y)
-			var z = y.indexOf('info')
-			var mycode = y.substring(z + 6)
-			var mycode2 = mycode.indexOf('</span>')
-			var mycode3 = mycode.substring(mycode + 9, mycode2) //alert(j+measure + ' is ' + mycode3)
-			demoArrayValues.push(mycode3);
-		}
+		// var myRe = new RegExp(myReString, 'g');
+		// var myArray
+		// while ((myArray = myRe.exec(str)) !== null) {
+		// 	const y = myArray.toString()
+		// 	var z = y.indexOf('info')
+		// 	var mycode = y.substring(z + 6)
+		// 	var mycode2 = mycode.indexOf('</span>')
+		// 	var mycode3 = mycode.substring(mycode + 9, mycode2) //alert(j+measure + ' is ' + mycode3)
+		// 	console.log(mycode3);
+		// 	demoArrayValues.push(mycode3);
+		// }
 		
 	}
 	
 	return demoArrayValues;
 }
 
-function getMeasures(measure) {
-	xmlhttp = new XMLHttpRequest();
-
-	xmlhttp.onreadystatechange = function() {
-		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-			//alert(xmlhttp.responseText)
-			//var str = xmlhttp.responseText.replace(/\s/g, '')
-			var str = xmlhttp.responseText
-			if (!str) {
-				return;
-			}
-
-			switch (measure) {
-				case "Phone(H)":
-					var myReString = '<span class="label">[\n\r\t]*\s*' + 'Phone' + '[(][' + 'H' + '][)]' + '(.|[\n])*'
-					break;
-				case "Phone(W)":
-					var myReString = '<span class="label">[\n\r\t]*\s*' + 'Phone' + '[(][' + 'W' + '][)]' + '(.|[\n])*'
-					break;
-				default:
-					var myReString = '<span class="label">[\n\r\t]*\s*' + measure + '(.|[\n])*'
-			}
-
-			var myRe = new RegExp(myReString, 'g');
-			var myArray
-			while ((myArray = myRe.exec(str)) !== null) {
-				y = myArray.toString()
-				//alert(y)
-				var z = y.indexOf('info')
-				var mycode = y.substring(z + 6)
-				var mycode2 = mycode.indexOf('</span>')
-				var mycode3 = mycode.substring(mycode + 9, mycode2) //alert(j+measure + ' is ' + mycode3)
-				demoArrayVal2[j] = mycode3
-			}
-		}
-	}
-	xmlhttp.open('GET', getMasterDemographicURL(), false);
-	xmlhttp.send();
-}
-
 function telLink(number){
-	var numberl = number.replace(/\D/g,'');
-	var l = "<a href='tel:9"+numberl+"'><span class='copyable'>"+number+"</span></a> &nbsp; ";
-	return l;
+	console.log(number.trim());
+	if(number.trim() == ""){
+		return "&nbsp;".repeat(20);
+	}
+	else {
+		const numberl = number.replace(/\D/g,'');
+		const l = "<a href='tel:9"+numberl+"'><span class='copyable'>"+number+"</span></a> &nbsp; ";
+		return l;
+	}
+
 }
 
