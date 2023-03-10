21467f54afe39810eda26e1ce9efd3e0c47be75c
Justin Wong <justin.wong098@gmail.com>
2023-03-09 18:10:52 -0800

    closeCortico: updated target to close, to match the new plugin update.

--- contentscripts/CloseCortico.js
+++ contentscripts/CloseCortico.js
@@ -32,16 +32,11 @@ async function checkEnabled_CloseCortico(){
 // Event listener
 //////////////////////////////////////////////////////////////////
 
-var theTarget2 = document.querySelectorAll(".tw-w-6")[0];
-theTarget2.addEventListener('click', function(theEvent) {
-	console.log("clicked t2");
-}, true);
 
-var theTarget3 = document.querySelectorAll(".tw-ml-1")[0];
-theTarget3.addEventListener('click', function(theEvent) {
-	console.log("clicked t3");
-}, true);
-
+/* 
+NOTE:
+- when trying to find the element target that closes the modal when clicked: Try the parents and children of the element that you think is being clicked.
+ */
 function keydownlistener_cortico(cortico_keyboardShortcuts){
 	const closeModal_enabled = 
 	cortico_keyboardShortcuts.cortico_shortcut_closeModal_enabled;
@@ -51,21 +46,20 @@ function keydownlistener_cortico(cortico_keyboardShortcuts){
 	window.addEventListener('keydown', function(theEvent) {
 		switch(true){
 			case closeModal_enabled && keybindingMatches(closeModal_keybinding, theEvent):
-				// const target1 = $("div.tw-font-sans:nth-child(3) > div:nth-child(1) > p:nth-child(2)");
-				// const target2 = $("div.tw-font-sans:nth-child(4) > div:nth-child(1) > p:nth-child(2)");
-				const target3 = $("div.tw-font-sans > div:nth-child(1) > p:nth-child(2)");
-				//:has(svg)
-				// target1.click();
-				// target2.click();
-				target3.click();
-				// console.log(target1);
-				// console.log(target2);
-				console.log(target3);
+				// const closeModalTarget1 = $(".tw-w-6 > path:nth-child(1)");
+				// const closeModalTarget2 = $(".tw-w-6");
+				const closeModalTarget3 = $(".tw-pr-3");
+				// const closeModalTarget2 = $("div.tw-font-sans > div:nth-child(1) > p:nth-child(2)");
+
+				// closeModalTarget1.click();
+				// closeModalTarget2.click();
+				closeModalTarget3.click();
 				break;
 		}
 	
 	}, false);
 }
+
 
 
 
