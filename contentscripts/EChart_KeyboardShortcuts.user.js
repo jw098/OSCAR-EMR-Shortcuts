ee43f9dd473431272d5ccbbe271150ff6c1b3202
Justin Wong <justin.wong098@gmail.com>
2023-03-19 14:30:02 -0700

    echart_keyboardshortcuts: updated targets to work with openOSP.

--- contentscripts/EChart_KeyboardShortcuts.user.js
+++ contentscripts/EChart_KeyboardShortcuts.user.js
@@ -69,44 +69,49 @@ function eChartPageHotkeys(eChart_mainWindow_keyboardShortcuts, theEvent){
 
 		case eChart_shortcut_meds_enabled && 
 			keybindingMatches(eChart_shortcut_meds_keybinding, theEvent):  // Medications
-
-			theTarget = document.evaluate("//div[@id='Rx']/div/div/h3/a",document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
+			const rxID = document.getElementById("Rx");
+			theTarget = document.evaluate("//div[@id='Rx']//a[contains(text(), 'Medication')]",document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
+			// theTarget = document.evaluate("//div[@id='Rx']/div/div/h3/a",document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
 			theTarget.click();
 			break;
 		case eChart_shortcut_eforms_enabled && 
 			keybindingMatches(eChart_shortcut_eforms_keybinding, theEvent):  // eForms
+			theTarget = document.evaluate("//div[contains(@id, 'menuTitleeforms')]/h3/a",document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
 
-			theTarget = document.evaluate("//div[@id='menuTitleeforms ']/h3/a",document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
+			// theTarget = document.evaluate("//div[@id='menuTitleeforms ']/h3/a",document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
 			theTarget.click();
 			break;
 		case eChart_shortcut_consults_enabled && 
 			keybindingMatches(eChart_shortcut_consults_keybinding, theEvent): // Consultation			
 
-			theTarget = document.evaluate("//div[@id='menuTitleconsultation ']/h3/a",document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
+			theTarget = document.evaluate("//div[contains(@id, 'menuTitleconsultation')]/h3/a",document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
 			theTarget.click();
 			break;  //"id('menuTitleconsultation ')/h3/a"  //"id('consultation')/div/div[2]/h3/a"  /html/body/div/div/div[4]/div[10]/div/div[2]/h3/a
 		case eChart_shortcut_tickler_enabled && 
 			keybindingMatches(eChart_shortcut_tickler_keybinding, theEvent):  // Tickler
 
-			theTarget = document.evaluate("//div[@id='menuTitletickler ']/h3/a",document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
+			theTarget = document.evaluate("//div[contains(@id, 'menuTitletickler')]/h3/a",document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
 			theTarget.click();
 			break;
 		case eChart_shortcut_SignSaveBill_enabled && 
 			keybindingMatches(eChart_shortcut_SignSaveBill_keybinding, theEvent):  // Sign, Save, and Bill
 
-			theTarget = document.evaluate("id('save')/span/input[contains(@src,'dollar-sign-icon.png')]",document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
+			theTarget = document.evaluate("id('save')/span/*[self::input or self::a][contains(@title,'Bill')]",document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
+
+			// theTarget = document.evaluate("id('save')/span/input[contains(@src,'dollar-sign-icon.png')]",document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
 			theTarget.click();
 			break;
 		case eChart_shortcut_save_enabled && 
 			keybindingMatches(eChart_shortcut_save_keybinding, theEvent):  // Save
-
-			theTarget = document.evaluate("id('save')/span/input[contains(@src,'media-floppy.png')]",document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
+			theTarget = document.getElementById("saveImg");
+			// theTarget = document.evaluate("id('save')/span/input[contains(@src,'media-floppy.png')]",document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
 			theTarget.click();
 			break;
 		case eChart_shortcut_signSave_enabled && 
 			keybindingMatches(eChart_shortcut_signSave_keybinding, theEvent):  // Sign and Save
+			theTarget = document.getElementById("signSaveImg");
+			// theTarget = document.evaluate("id('save')/span/input[contains(@src,'note-save.png')]",document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
 
-			theTarget = document.evaluate("id('save')/span/input[contains(@src,'note-save.png')]",document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
 			// setting focus to the button, and then clicking gives more consistent behavior. Otherwise, sometimes it gives the confirm dialog on close.
 			theTarget.focus();
 			setTimeout(function() {
@@ -119,9 +124,9 @@ function eChartPageHotkeys(eChart_mainWindow_keyboardShortcuts, theEvent){
 			break;
 		case eChart_shortcut_exit_enabled && 
 			keybindingMatches(eChart_shortcut_exit_keybinding, theEvent): // Exit
-
-			theTarget = document.evaluate("id('save')/span/input[contains(@src,'system-log-out.png')]",document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
-			theTarget.click();
+			window.close();
+			// theTarget = document.evaluate("id('save')/span/input[contains(@src,'system-log-out.png')]",document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
+			// theTarget.click();
 			break;
 
 	}
