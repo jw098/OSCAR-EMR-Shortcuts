13f0d390201df3bb3cc299e4131d5837e31ad00e
jw098 <justin.wong098@gmail.com>
2025-07-07 23:14:32 -0700

    [Consultations]: major refactor. use storage instead of XHR to get Past Medical History info.

---
+++ contentscripts/Consultations_PMHxInfo_ConsultsPage.js
@@ -0,0 +1,144 @@
+// ==UserScript==
+// @name						Consultations_KeyboardShortcuts
+// @namespace				oscar
+// @include					*/oscarConsultationRequest/ConsultationFormRequest.jsp*
+// @description			Within Consultations: Alt+1 to 'Submit Consultation Request'. Automatically pastes Past Medical history, Social history, and Family history to the Clinical information text area.
+// @require 				http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js
+// @grant						GM.setValue
+// @grant						GM.getValue
+// @grant						GM.deleteValue
+// ==/UserScript==
+//"*://*/*/oscarEncounter/ViewRequest.do*"
+ 
+///////////////////////////////////////////////////////////////////////////////////////////
+// Check Enabled
+///////////////////////////////////////////////////////////////////////////////////////////
+checkEnabled_Consultations_ConsultsPage();
+async function checkEnabled_Consultations_ConsultsPage(){
+	const isEnabled = await browser.storage.local.get('enabled');
+	console.log("Global enabled? " + isEnabled.enabled);
+	if(!isEnabled.enabled){
+		return;
+	}
+	else {
+		const consultationsObj = await browser.storage.local.get('consultations');
+		const consultations = consultationsObj.consultations;
+		const consultations_keyboardShortcuts = consultations.consultations_keyboardShortcuts;
+		if (consultations.postPatientAgeGender){
+			postPatientAgeGender();
+		}
+		if (consultations.postAllHistory){
+			getAllHistory();
+		}
+	}
+}
+
+
+////////////////////////////////
+// Patient Age and Gender
+////////////////////////////////
+
+
+
+function postPatientAgeGender(){
+	const theTarget = document.evaluate("//textarea[@name='reasonForConsultation']",document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
+	
+	const genderAndAge = document.querySelectorAll(".Header")[0].childNodes[2].nodeValue.trim().split("\t");
+	
+	const age2 = genderAndAge[1];
+
+	const birthDate = document.querySelectorAll("td.tite4:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(7) > td:nth-child(2)")[0].innerText;
+	const genderLetter = document.querySelectorAll("td.tite4:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(8) > td:nth-child(2)")[0].innerText;
+
+	const age = calcPatientAge(birthDate);
+	const gender = getGender(genderLetter);
+	console.log(age2);
+
+
+	theTarget.value = "Please see this " + age + gender + " for ";
+}
+
+function getGender(genderLetter){
+	let gender = "";
+	switch(genderLetter){
+		case "M":
+			gender = " male";
+			break;
+		case "F":
+			gender = " female";
+			break;
+		case "O":
+			gender = " (sex: Other)";
+			break;
+		case "U":
+			gender = " (sex: Undefined)";
+			break;
+		case "T":
+			gender = " transgender";
+			break;
+	}
+
+	return gender;
+}
+
+function calcPatientAge(birthDate){
+  const ageYears = yearsDiff(new Date(), new Date(birthDate));
+
+  if (ageYears >= 2){
+		return ageYears + "-year-old";
+  }
+  else {
+  	const ageMonths = monthsDiff(new Date(), new Date(birthDate));
+  	return ageMonths + "-month-old";
+  }
+
+  
+}
+
+function monthsDiff(d1, d2){
+	const diffTime = Math.abs(d1 - d2); // time difference in milliseconds
+	const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+	return Math.floor(diffDays/30);
+	// return (
+	//     d1.getMonth() -
+	//     d2.getMonth() +
+	//     12 * (d1.getFullYear() - d2.getFullYear())
+	//   );
+}
+
+function yearsDiff(d1, d2) {    
+	const diffTime = Math.abs(d1 - d2); // time difference in milliseconds
+	const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+  var timeFromEpoch = new Date(diffTime);  // the date diffTime miliseconds from the epoch (1970)
+  console.log(timeFromEpoch);
+  const yearsDiff = Math.abs(timeFromEpoch.getUTCFullYear() - 1970);
+	return yearsDiff;
+}
+
+/////////////////////////////////////////////////////////////////
+// Past Medical History, Social History, Family History
+/////////////////////////////////////////////////////////////////
+
+
+
+async function getAllHistory() {
+
+	// const [medHistory, socHistory, famHistory, otherMeds] = await Promise.all([getHistory(urlMedHistory()), getHistory(urlSocHistory()), getHistory(urlFamHistory()), getHistory(urlOtherMeds())]);
+	const medical_history_obj = await browser.storage.local.get('medical_history');
+	const social_history_obj = await browser.storage.local.get('social_history');
+	const family_history_obj = await browser.storage.local.get('family_history');
+
+	const medical_history_text = medical_history_obj.medical_history;	
+	const family_history_text = family_history_obj.family_history;
+	const social_history_text = social_history_obj.social_history;
+
+	const allHistory = "Past Medical History:\n" + medical_history_text + "\nSocial History:\n" + social_history_text + "\nFamily History:\n"  + family_history_text;
+	// console.log(allHistory);
+	
+	const clinInfoTextBox = document.getElementById('clinicalInformation');
+	clinInfoTextBox.value = allHistory;
+
+	const currentMedsBox = document.getElementById('currentMedications');
+	// currentMedsBox.value += "\n\nOther Meds:\n"  + otherMeds;
+}
---
+++ contentscripts/Consultations_PMHxInfo_EchartPage.js
@@ -0,0 +1,288 @@
+// ==UserScript==
+// @name						Consultations_KeyboardShortcuts
+// @namespace				oscar
+// @include					*/oscarConsultationRequest/ConsultationFormRequest.jsp*
+// @description			Within Consultations: Alt+1 to 'Submit Consultation Request'. Automatically pastes Past Medical history, Social history, and Family history to the Clinical information text area.
+// @require 				http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js
+// @grant						GM.setValue
+// @grant						GM.getValue
+// @grant						GM.deleteValue
+// ==/UserScript==
+//"*://*/*/oscarEncounter/ViewRequest.do*"
+ 
+///////////////////////////////////////////////////////////////////////////////////////////
+// Check Enabled
+///////////////////////////////////////////////////////////////////////////////////////////
+checkEnabled_Consultations_EchartPage();
+async function checkEnabled_Consultations_EchartPage(){
+	const isEnabled = await browser.storage.local.get('enabled');
+	console.log("Global enabled? " + isEnabled.enabled);
+	if(!isEnabled.enabled){
+		return;
+	}
+	else {
+		const consultationsObj = await browser.storage.local.get('consultations');
+		const consultations = consultationsObj.consultations;
+		const consultations_keyboardShortcuts = consultations.consultations_keyboardShortcuts;
+
+		if (consultations.postAllHistory){
+			CPPMutationObserver();
+		}
+	}
+}
+
+
+/////////////////////////////////////////////////////////////////
+// Past Medical History, Social History, Family History
+/////////////////////////////////////////////////////////////////
+
+/*
+PURPOSE:
+- use mutation observer to wait for the desired elements to load before trying to access them.
+
+NOTE:
+- unfortunately, a load event listener doesn't work, since the desired elements seem to load after the page 'loads'.
+*/
+function CPPMutationObserver(){
+	let mutationObserver = new MutationObserver(function(mutations) {
+
+		// mutations.forEach(function(mutation) {
+		// 	console.log(mutation);
+		// });
+
+		// console.log(mutations);
+		let socHxXPath = "//a[text()='Social History']";
+		let pMHxXPath = "//a[text()='Medical History']";
+		let famHxXPath = "//a[text()='Family History']";
+
+		let socHxBlock = document.evaluate(socHxXPath,document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;		
+		let pMHxBlock = document.evaluate(pMHxXPath,document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;		
+		let famHxBlock = document.evaluate(famHxXPath,document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;		
+		if (!!socHxBlock && !!pMHxBlock && !!famHxBlock){
+			mutationObserver.disconnect();
+
+			
+			getHistoryText("Medical History");
+			getHistoryText("Family History");
+			getHistoryText("Social History");
+		}
+	});
+
+	mutationObserver.observe(document.documentElement, {
+	  attributes: true,
+	  subtree: true,
+
+	  // characterData: true,
+	  // childList: true,
+	  // attributeOldValue: true,
+	  // characterDataOldValue: true
+	});
+}
+
+
+/*
+NOTE:
+- the location of Social History, Medical History, etc. aren't necessarily always in the same quadrant. So, need to find its div block first.
+- the text in each history block is extracted and saved to GreaseMonkey with GM.setValue. This allows the value to be accessed across tabs.
+  - GM.setValue and GM.getValue are asynchronous. @grant also needs to be set appropriately.
+*/
+function getHistoryText(history){
+	let historyXPath = "//a[text()='" + history+ "']";
+	let historyDivBlock = document.evaluate(historyXPath,document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;		
+	let historyDivBlockID = historyDivBlock.parentNode.parentNode.parentNode.id;
+
+	let historyNodeList = document.querySelectorAll('#'+historyDivBlockID+'> div:nth-child(3) > ul > li > span > a');
+	// console.log(historyNodeList);
+
+	let historyTextList = "";
+	historyNodeList.forEach(function(e){		
+		// console.log(e.innerText);
+		historyTextList += e.innerText + '\n';
+	});
+
+	(async () => {	
+		switch (history){
+			case "Social History":
+				// console.log("Social History:\n" + historyTextList);
+				await browser.storage.local.set({
+					social_history: historyTextList
+				});
+
+				const social_history_obj = await browser.storage.local.get('social_history');
+				console.log("stored ->\n" + social_history_obj.social_history);
+				break;
+			case "Medical History":
+				// console.log("Medical History:\n" + historyTextList);
+				await browser.storage.local.set({
+					medical_history: historyTextList
+				});				
+				const medical_history_obj = await browser.storage.local.get('medical_history');
+				console.log("stored ->\n" + medical_history_obj.medical_history);
+				break;
+			case "Family History":
+				// console.log("Family History:\n" + historyTextList);
+				await browser.storage.local.set({
+					family_history: historyTextList
+				});				
+				const family_history_obj = await browser.storage.local.get('family_history');
+				console.log("stored ->\n" + family_history_obj.family_history);	
+				break;			
+		}
+	})();
+	
+}
+
+/*
+PURPOSE:
+- get the values stored in GreaseMonkey from the previous tab (E-chart) and paste it in the clinical information text area.
+NOTES:
+- GM.getValue runs asynchronously, and seems to run after the window is loaded.
+*/
+(async () => {	
+	if(consultationPage.test(currentURL)) {
+		// console.log(await GM.getValue("socHx", "test"));
+		// console.log(await GM.getValue("pMHx", "test"));
+		// console.log(await GM.getValue("famHx", "test"));
+
+		let	pMHx = await GM.getValue("pMHx", "test");
+		let	socHx = await GM.getValue("socHx", "test");
+		let	famHx = await GM.getValue("famHx", "test");
+
+		pMHx = checkEmptyHistoryText(pMHx);
+		socHx = checkEmptyHistoryText(socHx);
+		famHx = checkEmptyHistoryText(famHx);
+
+		let allHistoryText = "Past Medical History:\n" + pMHx + 
+			"\nSocial History:\n" + socHx + "\nFamily History:\n" + famHx;
+
+		console.log(allHistoryText);
+		let clinInfoTextBox = document.getElementById('clinicalInformation');
+		clinInfoTextBox.value = allHistoryText;
+	}
+
+})();
+
+/*
+PURPOSE
+- if history text is empty string, return "<no data>"
+*/
+function checkEmptyHistoryText(historyText){
+	if (historyText == ""){
+		return "<no data>\n"
+	}
+	else {
+		return historyText;
+	}
+}
+
+
+/////////////////////////////////////////////////////////////////
+// Old stuff: Past Medical History, Social History, Family History
+/////////////////////////////////////////////////////////////////
+
+async function getAllHistory() {
+
+	get_history_top_right();
+
+
+	// const [medHistory, socHistory, famHistory, otherMeds] = await Promise.all([getHistory(urlMedHistory()), getHistory(urlSocHistory()), getHistory(urlFamHistory()), getHistory(urlOtherMeds())]);
+
+	// const allHistory = "Past Medical History:\n" + medHistory + "\nSocial History:\n" + socHistory + "\nFamily History:\n"  + famHistory;
+	// // console.log(allHistory);
+	
+	// const clinInfoTextBox = document.getElementById('clinicalInformation');
+	// clinInfoTextBox.value = allHistory;
+
+	// const currentMedsBox = document.getElementById('currentMedications');
+	// currentMedsBox.value += "\n\nOther Meds:\n"  + otherMeds;
+}
+
+
+
+async function getHistory(URL) {
+	const otherPageXMLText = await getXMLHTTP(URL);
+	const otherPageHTML = new DOMParser().parseFromString(otherPageXMLText, "text/html");
+  const historyDivList = otherPageHTML.querySelectorAll("body > div"); 
+
+  if (historyDivList.length == 0){
+  	return "<no data>\n"
+  }
+  else {
+		const historyText = getHistoryAsText(historyDivList);
+		console.log(historyText);
+
+		return historyText;
+  }
+
+}
+
+
+
+function getHistoryAsText(historyDivList){
+	let historyTextAllLines = "";
+	for (i = 0; i < historyDivList.length; i++){
+		const historyDiv = historyDivList[i];
+		const isArchived = historyDiv.children[1].innerText.includes("ARCHIVED");
+		if(isArchived){
+			// don't add text that is ARCHIVED.
+			continue;
+		}
+		const historyTextOneLine = historyDiv.children[0].innerText;
+		historyTextAllLines += historyTextOneLine + "\n";
+	}
+	return historyTextAllLines;
+}
+
+/////////////////////////////////////////////////////
+// get URL, URL elements
+/////////////////////////////////////////////////////
+
+
+function urlSocHistory(){
+	var newURL = getURLOrigin() + "CaseManagementEntry.do?method=issuehistory&demographicNo="+ getDemographicNum() + "&issueIds=65";
+
+	return newURL;
+}
+
+function urlMedHistory(){
+	var newURL = getURLOrigin() + "CaseManagementEntry.do?method=issuehistory&demographicNo="+ getDemographicNum() + "&issueIds=66";
+
+	return newURL;
+}
+
+function urlFamHistory(){
+	var newURL = getURLOrigin() + "CaseManagementEntry.do?method=issuehistory&demographicNo="+ getDemographicNum() + "&issueIds=69";
+
+	return newURL;
+}
+
+
+function urlEChart(){
+	var newURL = getURLOrigin() + "casemgmt/forward.jsp?action=view&demographicNo="+ getDemographicNum();
+
+	return newURL;
+}
+
+
+function urlOtherMeds(){
+	var newURL = getURLOrigin() + "CaseManagementEntry.do?method=issuehistory&demographicNo="+ getDemographicNum() + "&issueIds=64";
+
+	return newURL;
+}
+
+
+
+function getDemographicNum(){
+	var params = {}; //Get Params
+	if (location.search) {
+	    var parts = location.search.substring(1).split('&');
+	    for (var i = 0; i < parts.length; i++) {
+	        var nv = parts[i].split('=');
+	        if (!nv[0]) continue;``
+	        params[nv[0]] = nv[1] || true;
+	    }
+	}
+
+	return params.de;
+
+}
